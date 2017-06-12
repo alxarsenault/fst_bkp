f3398f38a38051aff513d94e5031cca50c37f06f
Alexandre Arsenault <aarsenault@ruizaero.com>
2017-06-12 00:08:51 -0400

    Set buffer_view data to const

--- include/fst/buffer_view.h
+++ include/fst/buffer_view.h
@@ -6,18 +6,13 @@ namespace fst {
 template <typename T>
 class buffer_view {
 public:
-	inline buffer_view(T* data = nullptr, std::size_t size = 0)
+	inline buffer_view(const T* data = nullptr, std::size_t size = 0)
 		: _data(data)
 		, _size(size)
 	{
 	}
 
 	buffer_view(const buffer_view&) = default;
-
-	inline T* data()
-	{
-		return _data;
-	}
 
 	inline const T* data() const
 	{
@@ -29,29 +24,14 @@ public:
 		return _size;
 	}
 
-	inline T& operator[](std::size_t index)
-	{
-		return _data[index];
-	}
-
 	inline const T& operator[](std::size_t index) const
 	{
 		return _data[index];
 	}
 
-	inline T* begin()
-	{
-		return _data;
-	}
-
 	inline const T* begin() const
 	{
 		return _data;
-	}
-
-	inline T* end()
-	{
-		return _data + _size;
 	}
 
 	inline const T* end() const
@@ -60,7 +40,7 @@ public:
 	}
 
 private:
-	T* _data;
+	const T* _data;
 	std::size_t _size;
 };
 } // fst.
