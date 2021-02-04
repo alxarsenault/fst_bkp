608b90302f6d1d9a0dae44340371b6efb1c0d43a
Alexandre Arsenault <alx.arsenault@gmail.com>
2021-02-03 23:31:03 -0500

    Add tests recources directory.

--- tests/file_buffer/file_buffer.cpp
+++ tests/file_buffer/file_buffer.cpp
@@ -4,12 +4,8 @@
 
 namespace {
 TEST(file_buffer, open) {
-
-  //  fst::file_buffer fb;
-  //  EXPECT_EQ(fb.open("/Users/alexarse/Desktop/TEST.txt"), true);
-
-  //  std::cout << fb.str() << std::endl;
-
-  fb.close();
+  fst::file_buffer fb;
+  EXPECT_EQ(fb.open(FST_TEST_RESOURCES_DIRECTORY "/test.txt"), true);
+  EXPECT_EQ(fb.str(), "Test");
 }
 } // namespace
