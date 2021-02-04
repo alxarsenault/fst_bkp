cbcbcce0c18f32bb5360972bf9de73ed78cea487
Alexandre Arsenault <alx.arsenault@gmail.com>
2021-02-04 01:37:33 -0500

    Add demangle.h

---
+++ include/fst/demangle.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <typeinfo>
+
+#if __has_include(<cxxabi.h>)
+#include <cxxabi.h>
+#define FST_HAS_DEMANGLE 1
+#else
+#define FST_HAS_DEMANGLE 0
+#endif
+
+namespace fst::util {
+#if FST_HAS_DEMANGLE
+template <typename T>
+std::string demangle() {
+  const char* mangled_name = typeid(T).name();
+  char buffer[1024];
+  unsigned int size = 1024;
+  int status;
+  char* res = abi::__cxa_demangle(mangled_name, buffer, &size, &status);
+
+  if (status == 0) {
+    return std::string(res, size);
+  }
+
+  return mangled_name;
+}
+
+std::string demangle(const char* mangled_name) {
+  char buffer[1024];
+  unsigned int size = 1024;
+  int status;
+  char* res = abi::__cxa_demangle(mangled_name, buffer, &size, &status);
+
+  if (status == 0) {
+    return std::string(res, size);
+  }
+
+  return mangled_name;
+}
+#else
+template <typename T>
+std::string demangle() {
+  return mangled_name;
+}
+
+std::string demangle(const char* mangled_name) { return typeid(T).name(); }
+#endif
+} // namespace fst::util.
