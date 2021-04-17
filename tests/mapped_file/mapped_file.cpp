209b743b7acace6d877e293510de9d2de4cb0210
Alexandre Arsenault <alx.arsenault@gmail.com>
2021-04-17 02:29:30 -0400

    Try to fix build on windows.

--- tests/mapped_file/mapped_file.cpp
+++ tests/mapped_file/mapped_file.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 #include <string_view>
 #include "fst/mapped_file.h"
+#include "fst/print.h"
 
 namespace {
 TEST(mapped_file, open) {
   fst::mapped_file fb;
+  fst::print(FST_TEST_RESOURCES_DIRECTORY "/test.txt");
   EXPECT_EQ(fb.open(FST_TEST_RESOURCES_DIRECTORY "/test.txt"), true);
   EXPECT_EQ(fb.str(), "Test");
 }
